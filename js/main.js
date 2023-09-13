20cbbc2b5a8fcb0708e63885f3b1aecffcad5fa2
Ethan <unclebigbay@hashnode.com>
2023-09-13 13:31:43 +0200

    first-commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// exo 1.6
+// let a = 0;
+// const tmp = a;
+// let b = a;
+// a = b;
+// b = tmp;
+// console.log(a, b);
+
+// exo 3.2
+// let n1 = -5;
+// let n2 = -10;
+
+// if ((n1 < 0 && n2 < 0) || (n1 > 0 && n2 > 0)) {
+//   console.log("ce produit est positif");
+// } else {
+//   console.log("ce produit est négatif");
+// }
+
+// exo 3.3
+// let nom1 = "Abd";
+// let nom2 = "Abc";
+// let nom3 = "Ethan";
+
+// if (nom1 < nom2 && nom2 < nom3) {
+//   console.log("c'est noms sont dans l'ordre alphabétique");
+// } else {
+//   console.log("c'est noms sont PAS dans l'ordre alphabétique");
+// }
+
+// exo 3.6
+const age = 13;
+
+if (age >= 12) {
+  console.log("Cadet");
+} else if (age >= 10) {
+  console.log("Minime");
+} else if (age >= 8) {
+  console.log("Pupille");
+} else if (age >= 6) {
+  console.log("Poussin");
+} else {
+  console.log("tu na pas l'age");
+}
